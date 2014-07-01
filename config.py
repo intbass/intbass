4e9f03828244f4de06e4f8f37fbb082abaa0d7f6
DDub <ddub@intbass.com>
2014-07-01 14:03:42 -0400

    Setup config environment variables
    Allows an external config file to be set which can contain installation
    specific settings to be stored
    
    $ cat config.py | sed "/^SECRET_KEY = / s/=.*/= $RANDOM/" > ~/intbass-settings.py
    $ INTBASS_SETTINGS=~/intbass-settings.py python run.py
    
    The default config will load in "development mode" that is which minimal
    external dependencies and on the local machine

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-CSRF_ENABLED=True
-SECRET_KEY='My Super Secret key'
-FILE_PATH='/home/incognitus/Music'
+DEBUG = True
+CSRF_ENABLED = True
+SECRET_KEY = 'My Super Secret key'
+FILE_PATH = os.path.join(basedir, '/Music')
+HOST = '0.0.0.0'
