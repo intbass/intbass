86c2ab7cb32450b0b1c4bd5a9a73e993b6c27ed6
DDub <ddub@intbass.com>
2014-07-01 15:03:43 -0400

    First pass add admin user on db_create
    Asks for username and email address
    Need to ask for a password, silently with confirmation

--- db_create.py
+++ db_create.py
@@ -3,10 +3,33 @@ from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
+from app.models import Users
 import os.path
+
+
+def input(prompt):
+    # Fix Python 2.x.
+    try:
+        input = raw_input
+    except NameError:
+        pass
+
+    value = ''
+    while value == '':
+        value = input('{}: '.format(prompt))
+    return value
+
+
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+admin = Users(name=input('Admin username'),
+              location='Earth',
+              email=input('Admin email address'),
+              )
+admin.set_password('password')
+db.session.add(admin)
+db.session.commit()
