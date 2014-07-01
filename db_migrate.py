4cd4c9ae7fe5143e637d066aa8f8fc4ea43571c1
DDub <ddub@intbass.com>
2014-07-01 16:21:51 -0400

    Use app settings in db_migrate

--- db_migrate.py
+++ db_migrate.py
@@ -1,16 +1,16 @@
 #!bin/python
 import imp
 from migrate.versioning import api
-from app import db
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
-migration = SQLALCHEMY_MIGRATE_REPO + '/versions/%03d_migration.py' % (api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO) + 1)
+from app import app, db
+db_repo = app.config['SQLALCHEMY_MIGRATE_REPO']
+db_uri = app.config['SQLALCHEMY_DATABASE_URI']
+migration = db_repo + '/versions/%03d_migration.py' % (api.db_version(db_uri, db_repo) + 1)
 tmp_module = imp.new_module('old_model')
-old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+old_model = api.create_model(db_uri, db_repo)
 exec old_model in tmp_module.__dict__
-script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
+script = api.make_update_script_for_model(db_uri, db_repo, tmp_module.meta, db.metadata)
 open(migration, "wt").write(script)
-api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+api.upgrade(db_uri, db_repo)
 print 'New migration saved as ' + migration
-print 'Current database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
+print 'Current database version: ' + str(api.db_version(db_uri, db_repo))
 
