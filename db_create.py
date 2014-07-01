9ca8fd5d0bf233134c36995360d586472d1664f1
DDub <ddub@intbass.com>
2014-07-01 15:15:43 -0400

    Prompt for password also

--- db_create.py
+++ db_create.py
@@ -5,6 +5,7 @@ from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
 from app.models import Users
 import os.path
+from getpass import getpass
 
 
 def input(prompt):
@@ -20,16 +21,21 @@ def input(prompt):
     return value
 
 
+name = input('Admin username')
+email = input('Admin email address')
+password = ''
+confirm = 'no'
+while password != confirm:
+    password = getpass(prompt='Admin password: ')
+    confirm = getpass(prompt='Confirm admin password: ')
+
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
-admin = Users(name=input('Admin username'),
-              location='Earth',
-              email=input('Admin email address'),
-              )
-admin.set_password('password')
+admin = Users(name=name, location='Earth', email=email)
+admin.set_password(password)
 db.session.add(admin)
 db.session.commit()
