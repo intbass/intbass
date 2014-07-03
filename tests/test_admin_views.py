577b40820bf2ab18e087f29a3f8698498d76131f
DDub <ddub@intbass.com>
2014-07-03 19:09:53 -0400

    tidy admin tests

--- tests/test_admin_views.py
+++ tests/test_admin_views.py
@@ -4,16 +4,15 @@ from tests.user_test import UserTestCase
 
 
 class TestAnonAdminView(BassTestCase):
-    def test_admin_index(self):
-        rv = self.app.get('/admin/')
-        assert rv.status_code == 404
+    def test_denied(self):
+        for url in '', 'users/', 'users/edit/1', 'files/', 'files/a':
+            rv = self.app.get('/admin/{}'.format(url))
+            assert rv.status_code == 404
 
-    def test_users_index(self):
-        rv = self.app.get('/admin/users/')
-        assert rv.status_code == 404
 
 class TestUserAdminView(UserTestCase, TestAnonAdminView):
     pass
+
 
 class TestAdminView(AdminTestCase):
     def test_users_index(self):
