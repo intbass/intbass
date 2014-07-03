9ff0b06df3f2582ce1922fa3264840be7d7ce881
DDub <ddub@intbass.com>
2014-07-02 21:09:24 -0400

    Change app identifier

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask('intbass')
 app.config.from_object('config')
 app.config.from_envvar("INTBASS_SETTINGS", silent=True)
 db = SQLAlchemy(app)
