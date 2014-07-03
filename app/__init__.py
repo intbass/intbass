49b1aacb93744ef16f86a55a81ea196090142671
DDub <ddub@intbass.com>
2014-07-02 21:10:19 -0400

    Add anonymous users with no capabilities

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,9 @@ app = Flask('intbass')
 app.config.from_object('config')
 app.config.from_envvar("INTBASS_SETTINGS", silent=True)
 db = SQLAlchemy(app)
-
 lm = LoginManager()
 lm.login_view = "login"
 lm.init_app(app)
 
 from app import views, models
-
+lm.anonymous_user = models.AnonymousUser
