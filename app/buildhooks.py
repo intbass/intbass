d54f27cdd2d6bbacd9e265d616f4a1c6be7b5c2b
DDub <ddub@intbass.com>
2014-07-09 00:45:29 -0400

    Sort out logging

--- app/buildhooks.py
+++ app/buildhooks.py
@@ -23,39 +23,39 @@ def build_travis():
     token = app.config('TRAVIS_TOKEN')
     ghrepo = app.config('GITHUB_REPO')
     if token is None or ghrepo is None:
-        error('Travis-CI API options are not set')
+        app.logger.error('Travis-CI API options are not set')
         abort(500)
     auth = app.config('GITHUB_REPO') + app.config('TRAVIS_TOKEN')
     repo_url = app.config('GITHUB_INSTALL') + app.config('GITHUB_REPO')
     # Authenticate
     if sha256(auth).hexdigest() != request.headers.get('Authorization'):
-        info('Failed Travis CI authentication')
+        app.logger.info('Failed Travis CI authentication')
         abort(401)
     # Parse input
     try:
         note = json.loads(request.form['payload'].encode('ascii'))
     except ValueError:
-        critical('Failed travis-ci payload parsing')
+        app.logger.critical('Failed travis-ci payload parsing')
         abort(400)
     # check if it is relevant
     if 'repository' not in note or note['repository'] is None:
-        warn('No repostory info in travis webhook')
+        app.logger.warn('No repostory info in travis webhook')
         abort(400)
     # set a precondition of it matching the configured repo
     if note['repository']['url'] != repo_url:
         message = 'Not our repository: {}'.format(note['repository']['url'])
-        info(message)
+        app.logger.info(message)
         # HTTP Error 412 - Precondition failed
         # seems fair
         return Response(message, 412)
 
     # Do something?
-    info(json.dumps(note))
+    app.logger.info(json.dumps(note))
 
     message = note['status_message'] if 'status_message' in note else None
     if 'result_message' in note and message is None:
         message = note['result_message']
     if message is None:
-        warn('Travis webhook sent without any message')
+        app.logger.warn('Travis webhook sent without any message')
         abort(400)
     return '{message}'.format(**note)
--- site.wsgi
+++ site.wsgi
@@ -4,12 +4,12 @@ import logging
 dir = os.path.dirname(os.path.abspath(__file__))
 activate_this = '%s/bin/activate_this.py' % dir
 execfile(activate_this, dict(__file__=activate_this))
-#logging.basicConfig(filename='/home/intlbass/logs/flask.log')
-#logger = logging.getLogger()
 sys.path.insert(0, dir)
-#application.logger.addHandler(logger)
-# WSGI sends environmnet vairables on a per request basis in req_environ
+# WSGI sends environment variables on a per request basis in req_environ
 def application(req_environ, start_response):
     os.environ['INTBASS_SETTINGS'] = req_environ['INTBASS_SETTINGS']
     from app import app
+    stdhandler = logging.StreamHandler(stream=sys.stderr)
+    stdhandler.setLevel(logging.DEBUG)
+    app.logger.addHandler(stdhandler)
     return app(req_environ, start_response)
