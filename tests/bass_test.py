cc884dce25331e492d3bba6af843b193c3b66ed2
ddub <git@dub.to>
2016-02-16 21:00:10 -0500

    A bit more info

--- tests/bass_test.py
+++ tests/bass_test.py
@@ -1,15 +1,18 @@
 from app import app, db
 from tests import config
 import unittest
+from logging import warn
 
 
 class BassTestCase(unittest.TestCase):
     """A base test case for international bass."""
 
     def setUp(self):
+        warn('setup: {}'.format(self))
         app.config.from_object(config)
         self.app = app.test_client()
         db.create_all()
+        warn('complete setup: {}'.format(self))
 
     def tearDown(self):
         db.session.remove()
